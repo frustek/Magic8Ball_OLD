7c5015d06660d0ccba06bedf048d2d8255aa878e
Adam Mroz <MrozAdam1992@gmail.com>
2022-06-21 21:21:43 +0200

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Adam Mroz on 21/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
